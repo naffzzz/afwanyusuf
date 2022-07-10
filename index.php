4a7e1b46673e125c587981283bdce70b51746db4
unknown <naffzzz@gmail.com>
2022-07-10 16:45:55 +0700

    init

---
+++ index.php
@@ -0,0 +1,336 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="fontawesome/css/fontawesome.css">
+    <link rel="stylesheet" href="fontawesome/css/brands.css">
+    <link rel="stylesheet" href="fontawesome/css/solid.css">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Afwan Yusuf Bahtiar</title>
+  </head>
+  <body class="body-background" onload="reveal()">
+    <!-- 4 - contained in center example -->
+    <nav class="navbar navbar-expand-sm navbar-light bg-white fixed-top slideInFromTopDef">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample11" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+      </button>
+        <div class="mx-auto d-sm-flex d-block flex-sm-nowrap">
+            <div class="collapse navbar-collapse text-center" id="navbarsExample11">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#" id="biographyBtn">Biography</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#" id="educationBtn">Education</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#" id="workExperiencesBtn">Work Experiences</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#" id="organizationsBtn">Organizations</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#" id="contactMeBtn">Contact Me</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <hr>
+
+    <div class="container mt-5">
+      <center class="reveal slideInFromBottom" id="biography"><h1 class="subtitle2">Biography <i class="fa-solid fa-briefcase"></i></h1></center>
+      <div class="row mb-5 reveal slideInFromBottom">
+        <div class="col bg-white text-justify">
+          <span>Afwan Yusuf Bahatiar was born in Kediri, Indonesia. He received the Bachelor degree in Management from Islamic University of Maulana Malik Ibrahim Malang (UIN Malang), Malang, Indonesia.
+            He is focusing on human resource consentration.</span>
+        </div>
+      </div>
+
+      <center class="reveal slideInFromBottom" id="education">
+        <h1 class="subtitle">Education <i class="fa-solid fa-school"></i></h1>
+      </center>
+      <div class="row text-center mb-5">
+        <div class="col">
+          <div class="card border rounded reveal slideInFromLeft">
+            <div class="text-info">
+              <span class="fa-stack fa-4x mb-2 mt-2">
+                <i class="fa fa-circle fa-stack-2x icon-background"></i>
+                <i class="fa-solid fa-graduation-cap  fa-stack-1x"></i>
+              </span>
+            </div>
+            <h4>Bachelor Degree</h4>
+            2015 - 2021<br>
+            (Management) Islamic University Maulana Malik Ibrahim, Malang
+          </div>
+        </div>
+      </div>
+      <div class="row text-center mb-5">
+        <div class="col">
+          <div class="card border rounded reveal slideInFromRight">
+            <div class="text-info">
+              <span class="fa-stack fa-4x mb-2 mt-2">
+                <i class="fa fa-circle fa-stack-2x icon-background"></i>
+                <i class="fa-solid fa-graduation-cap  fa-stack-1x"></i>
+              </span>
+            </div>
+            <h4>Senior Highschool</h4>
+            August 2014 - August 2018<br>
+            (Social) Islamic Senior Highschool Tambakberas, Jombang
+          </div>
+        </div>
+      </div>
+      <div class="row text-center mb-5">
+        <div class="col">
+          <div class="card border rounded reveal slideInFromLeft">
+            <div class="text-info">
+              <span class="fa-stack fa-4x mb-2 mt-2">
+                <i class="fa fa-circle fa-stack-2x icon-background"></i>
+                <i class="fa-solid fa-graduation-cap  fa-stack-1x"></i>
+              </span>
+            </div>
+            <h4>Junior Highschool</h4>
+            2009 - 2012<br>
+            Islamic Junior Highschool 1 Kediri, Kediri
+          </div>
+        </div>
+      </div>
+      <div class="row text-center mb-5">
+        <div class="col">
+          <div class="card border rounded reveal slideInFromRight">
+            <div class="text-info">
+              <span class="fa-stack fa-4x mb-2 mt-2">
+                <i class="fa fa-circle fa-stack-2x icon-background"></i>
+                <i class="fa-solid fa-graduation-cap  fa-stack-1x"></i>
+              </span>
+            </div>
+            <h4>Elementary School</h4>
+            2003 - 2009<br>
+            Islamic Elementary School Miftahul Ulum, Kediri
+          </div>
+        </div>
+      </div>
+      <div class="row text-center mb-5">
+        <div class="col">
+          <div class="card border rounded reveal slideInFromLeft">
+            <div class="text-info">
+              <span class="fa-stack fa-4x mb-2 mt-2">
+                <i class="fa fa-circle fa-stack-2x icon-background"></i>
+                <i class="fa-solid fa-graduation-cap  fa-stack-1x"></i>
+              </span>
+            </div>
+            <h4>pre-primary school</h4>
+            2001 - 2003<br>
+            RA Perwanida Sidomulyo, Kediri
+          </div>
+        </div>
+      </div>
+
+      <center id="workExperiences" class="reveal slideInFromBottom"><h1 class="subtitle2">Work Experiences <i class="fa-solid fa-briefcase"></i></h1></center>
+      <div class="row text-center mb-5 reveal slideInFromBottom">
+        <div class="col bg-white">
+          <div class="d-flex justify-content-between">
+            <div>
+              <span><i class="fa-solid fa-user-gear text-info"></i> Intern Employee</span>
+            </div>
+            <div>
+              <span>January 2020 - May 2020</span>
+            </div>
+          </div>
+          <div class="d-flex justify-content-between">
+            <div>
+              <span>KPPS BMT PETA</span>
+            </div>
+            <div>
+              <span>Blitar, Indonesia</span>
+            </div>
+          </div>
+          <hr>
+          <div class="d-flex justify-content-between">
+            <div>
+              <span><i class="fa-solid fa-chalkboard-user text-success"></i> Intern Employee</span>
+            </div>
+            <div>
+              <span>Sept 2018 - Sept 2018</span>
+            </div>
+          </div>
+          <div class="d-flex justify-content-between">
+            <div>
+              <span>Mitra Pinasthika Mustika Finance </span>
+            </div>
+            <div>
+              <span>Malang, Indonesia</span>
+            </div>
+          </div>
+          <hr>
+          <div class="d-flex justify-content-between">
+            <div>
+              <span><i class="fa-solid fa-user-tie text-success"></i> Intern Employee</span>
+            </div>
+            <div>
+              <span>January 2018 - March 2018</span>
+            </div>
+          </div>
+          <div class="d-flex justify-content-between">
+            <div>
+              <span>PT. Victory Internasional Futures </span>
+            </div>
+            <div>
+              <span>Malang, Indonesia</span>
+            </div>
+          </div>
+          <hr>
+        </div>
+      </div>
+
+      <center id="organizations" class="reveal slideInFromBottom"><h1 class="text-success">Organizations <i class="fa-solid fa-users-line"></i></h1></center>
+      <div class="row text-center mb-5 reveal slideInFromBottom">
+        <div class="col bg-white">
+          <div class="d-flex justify-content-between">
+            <div>
+              <span><i class="fa-solid fa-user-gear text-info"></i> Organizer</span>
+            </div>
+            <div>
+              <span>2018 - 2019</span>
+            </div>
+          </div>
+          <div class="d-flex justify-content-between">
+            <div>
+              <span>PMII Komisariat Sunan Ampel </span>
+            </div>
+            <div>
+              <span>Malang, Indonesia</span>
+            </div>
+          </div>
+          <hr>
+          <div class="d-flex justify-content-between">
+            <div>
+              <span><i class="fa-solid fa-chalkboard-user text-success"></i> Organizer</span>
+            </div>
+            <div>
+              <span>2018 - 2019</span>
+            </div>
+          </div>
+          <div class="d-flex justify-content-between">
+            <div>
+              <span>Dewan Eksekutif Mahasiswa (DEMA) Fakultas Ekonomi </span>
+            </div>
+            <div>
+              <span>Malang, Indonesia</span>
+            </div>
+          </div>
+          <hr>
+          <div class="d-flex justify-content-between">
+            <div>
+              <span><i class="fa-solid fa-user-tie text-success"></i> Organizer</span>
+            </div>
+            <div>
+              <span>2016 - 2018</span>
+            </div>
+          </div>
+          <div class="d-flex justify-content-between">
+            <div>
+              <span>PMII Rayon Ekonomi Moh. Hatta Malang </span>
+            </div>
+            <div>
+              <span>Malang, Indonesia</span>
+            </div>
+          </div>
+          <hr>
+          <div class="d-flex justify-content-between">
+            <div>
+              <span><i class="fa-solid fa-user-tie text-success"></i> Organizer</span>
+            </div>
+            <div>
+              <span>2016 - 2018</span>
+            </div>
+          </div>
+          <div class="d-flex justify-content-between">
+            <div>
+              <span>Himpunan Mahasiswa Jurusan (HMJ) Manajemen </span>
+            </div>
+            <div>
+              <span>Malang, Indonesia</span>
+            </div>
+          </div>
+          <hr>
+        </div>
+      </div>
+
+    </div>
+
+    <footer class="footer1 pb-5 pt-5">
+      <div id="contactMe" class="container">
+        <center>
+          <a href="https://www.facebook.com/af.pittuhhlass" style="text-decoration: none;" class="text-white" target="_blank"><i class="fa-brands fa-facebook fa-xl"></i> Facebook </a>
+          <a href="https://www.instagram.com/afwanyusuf/" style="text-decoration: none;" class="text-white" target="_blank"><i class="fa-brands fa-instagram fa-xl"></i> Instagram </a>
+          <a href="https://www.linkedin.com/in/nafa-zulfa-ab85a1119/" style="text-decoration: none;" class="text-white" target="_blank"><i class="fa-brands fa-linkedin fa-xl"></i> Linkedin </a>
+          <a href="https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=afwanbahtiar17@gmail.com" style="text-decoration:none;" class="text-white" target="_blank"><i class="fa-solid fa-at fa-xl"></i> Email </a>
+        </center>
+      </div>
+    </footer>
+
+    <!-- Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script>
+    function reveal() {
+      var reveals = document.querySelectorAll(".reveal");
+
+      for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+
+        if (elementTop < windowHeight - elementVisible) {
+          reveals[i].classList.add("active");
+        }
+        // else {
+        //   reveals[i].classList.remove("active");
+        // }
+      }
+    }
+    window.addEventListener("scroll", reveal);
+    </script>
+
+    <script>
+        $( document ).ready(function() {
+          $("#biographyBtn").click(function() {
+              $('html,body').animate({
+                  scrollTop: $("#biography").offset().top -50},
+                  'slow');
+          });
+
+          $("#educationBtn").click(function() {
+              $('html,body').animate({
+                  scrollTop: $("#education").offset().top -50},
+                  'slow');
+          });
+
+          $("#workExperiencesBtn").click(function() {
+              $('html,body').animate({
+                  scrollTop: $("#workExperiences").offset().top -50},
+                  'slow');
+          });
+
+          $("#organizationsBtn").click(function() {
+              $('html,body').animate({
+                  scrollTop: $("#organizations").offset().top -50},
+                  'slow');
+          });
+
+          $("#contactMeBtn").click(function() {
+              $('html,body').animate({
+                  scrollTop: $("#contactMe").offset().top -50},
+                  'slow');
+          });
+        });
+    </script>
+  </body>
+</html>
